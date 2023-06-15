3ba4135274761b4843b078b098c132bd8734ba62
Akash Chauhan <meachauhan@gmail.com>
2023-06-15 18:51:17 +0530

    Initial Draft of SpringBoot with MongoDB

---
+++ src/test/java/com/JobListingApplicationTests.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootTest
+
+class JobListingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
